26d02d7c1acc934c50ee2f7de771d12e73367569
torezf <tortaywe@gmail.com>
2025-08-20 14:27:09 +0700

    fix: break word in text field , no match route go to root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "todolists#index"
 
-  resources :todolists do
-  end
+  resources :todolists
 
   get "/brag" => "pages#index"
+
+  match "*path", to: redirect("/"), via: :all
 end
